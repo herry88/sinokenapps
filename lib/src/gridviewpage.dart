a79cd2776f40ee7cfad58169b2dfef23be77e133
Herry Prasetyo <herry081288@gmail.com>
2022-08-02 21:40:50 +0700

    newDesain

---
+++ lib/src/gridviewpage.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class GridViewPage extends StatelessWidget {
+  Item usulkp = Item();
+
+  @override
+  Widget build(BuildContext context) {
+    List<Item> myList = [usulkp, usulpensiun, usulk3];
+    return Flexible(
+      child: GridView(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 18.0,
+          mainAxisSpacing: 18.0,
+        ),
+      ),
+    );
+  }
+}
+
+class Item {
+  String? title;
+  String? subTitle;
+  String? event;
+  String? img;
+  Item({
+    this.title,
+    this.subTitle,
+    this.event,
+    this.img,
+  });
+}
